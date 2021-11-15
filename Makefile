6bc9b9096bb115ff979862ef11efd63ab6a2ac4b
Nao1215 <n.chika156@gmail.com>
2021-11-15 22:26:18 +0900

    Makefile is self-documented and a help message is displayed if no target is specified when executing make. And, I add jail-target for Makefile. Jail-target make rootfs at /tmp/mimixbox/jail.

--- Makefile
+++ Makefile
@@ -1,23 +1,33 @@
-build: deps
+build: deps ## Build mimixbox and make man-pages
 	go build -o mimixbox cmd/mimixbox/main.go
 	$(MAKE) doc
 
-clean:
+clean: ## Clean project
 	-rm mimixbox
 	-rm -rf vendor
 	-rm cover.*
 	-rm -rf release
 	-find . -name "*.1.gz" | xargs rm -f
 
-doc:
+doc: ## Make man-pages
 	./scripts/mkManpages.sh
 
-install:
+install: ## Install mimixbox and man-pages on your system
 	./scripts/installer.sh
 
-release:
+jail:  ## Make jail environment for testing chroot/ischroot
+	./scripts/mkJailForDebianFamily.sh
+
+release: ## Make release files.
 	./scripts/release.sh
 
-deps:
+deps: ## Dependency resolution for build
 	dep ensure
 	go mod vendor
+
+.DEFAULT_GOAL := help
+.PHONY: build clean doc install jail release deps
+ 
+help:  
+	@grep -E '^[0-9a-zA-Z_-]+[[:blank:]]*:.*?## .*$$' $(MAKEFILE_LIST) | sort \
+	| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[1;32m%-15s\033[0m %s\n", $$1, $$2}'
---
+++ scripts/mkJailForDebianFamily.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# [Description]
+# This shell script make rootfs(jail). Rootfs is created as /tmp/mimixbox/jail
+# by debootstrap command. It is used as the test environment for chroot
+# command and ischroot command.
+# This script cannot create rootfs on RHEL-based distributions.
+# Only work for Debian-based one.
+ROOT_DIR=$(git rev-parse --show-toplevel)
+SCRIPT_NAME=$(basename $0)
+JAIL="/tmp/mimixbox/jail"
+source ${ROOT_DIR}/scripts/libshell.sh
+
+function isDebian() {
+    if [ ! -f /etc/debian_version ]; then
+        errMsg "This script can only be used with Debian-based distributions."
+        exit 1
+    fi 
+}
+
+function hasDebootstrap() {
+    which debootstrap > /dev/null
+    local exit_status="$?"
+    if [ "${exit_status}" != "0" ]; then
+        errMsg "This script use debootstrap command."
+        errMsg "[How to install]"
+        errMsg " $ sudo apt install debootstrap"
+        exit 1
+    fi
+}
+
+function mkJail() {
+    sudo rm -rf ${JAIL}
+    sudo debootstrap bullseye ${JAIL} http://deb.debian.org/debian
+}
+
+function cpMimixboxIfNeeded() {
+    if [ -f ${ROOT_DIR}/mimixbox ]; then
+        cp ${ROOT_DIR}/mimixbox ${JAIL}/usr/bin/.
+    fi 
+}
+
+isDebian
+hasDebootstrap
+IS_ROOT=$(isRoot)
+if [ "$IS_ROOT" = "1" ]; then
+    errMsg "You are not root user."
+    exit 1
+fi
+
+mkJail
+cpMimixboxIfNeeded
