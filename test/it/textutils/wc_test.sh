db8a8ccfe2ec33cb48c638ea22974ed9d1d93f75
Nao1215 <n.chika156@gmail.com>
2021-12-01 19:37:09 +0900

    Revert wc tests

--- test/it/textutils/wc_test.sh
+++ test/it/textutils/wc_test.sh
@@ -9,20 +9,20 @@ Setup() {
     export TEST_FILE_METAL=/tmp/mimixbox/it/metal.txt
     export EMPTY_FILE=/tmp/mimixbox/it/empty.txt
 
-    mkdir -p /tmp/mimixbox/it
+    mkdir -p ${TEST_DIR}
 
-    echo "NieR Replicant ver.1.22474487139..." > /tmp/mimixbox/it/game.txt
-    echo "NieR:Automata" >>  /tmp/mimixbox/it/game.txt
-    echo "The Legend of Zelda: Majora's Mask" >>  /tmp/mimixbox/it/game.txt
-    echo "KICHIKUOU RANCE" >>  /tmp/mimixbox/it/game.txt
-    echo "DARK SOULS" >> /tmp/mimixbox/it/game.txt
-    echo "SHADOW HEARTS" >>  /tmp/mimixbox/it/game.txt
+    echo "NieR Replicant ver.1.22474487139..." > ${TEST_FILE_GAMENAME}
+    echo "NieR:Automata" >>  ${TEST_FILE_GAMENAME}
+    echo "The Legend of Zelda: Majora's Mask" >>  ${TEST_FILE_GAMENAME}
+    echo "KICHIKUOU RANCE" >>  ${TEST_FILE_GAMENAME}
+    echo "DARK SOULS" >>  ${TEST_FILE_GAMENAME}
+    echo "SHADOW HEARTS" >>  ${TEST_FILE_GAMENAME}
 
-    echo "MEGADETH" > /tmp/mimixbox/it/metal.txt
-    echo "GALNERYUS" >> /tmp/mimixbox/it/metal.txt
-    echo "SYSTEM OF A DOWN" >> /tmp/mimixbox/it/metal.txt
+    echo "MEGADETH" > ${TEST_FILE_METAL}
+    echo "GALNERYUS" >> ${TEST_FILE_METAL}
+    echo "SYSTEM OF A DOWN" >> ${TEST_FILE_METAL}
     
-    touch /tmp/mimixbox/it/empty.txt
+    touch ${EMPTY_FILE}
 }
 
 CleanUp() {
@@ -30,37 +30,37 @@ CleanUp() {
     export TEST_FILE_METAL=/tmp/mimixbox/it/metal.txt
     export EMPTY_FILE=/tmp/mimixbox/it/empty.txt
 
-    rm  /tmp/mimixbox/it/empty.txt /tmp/mimixbox/it/game.txt /tmp/mimixbox/it/metal.txt
+    rm  ${TEST_FILE_GAMENAME} ${EMPTY_FILE} ${TEST_FILE_METAL}
 }
 
 TestWcWithNoOption() {
     export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
-    wc /tmp/mimixbox/it/game.txt
+    wc ${TEST_FILE_GAMENAME}
 }
 
 TestWcWithLinesOption() {
     export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
-    wc -l /tmp/mimixbox/it/game.txt
+    wc -l ${TEST_FILE_GAMENAME}
 }
 
 TestWcWithBytesOption() {
     export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
-    wc -c /tmp/mimixbox/it/game.txt
+    wc -c ${TEST_FILE_GAMENAME}
 }
 
 TestWcWithMaxLineLengthOption() {
     export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
-    wc -L /tmp/mimixbox/it/game.txt
+    wc -L ${TEST_FILE_GAMENAME}
 }
 
 TestWcReadingEmptyFile() {
     export EMPTY_FILE=/tmp/mimixbox/it/empty.txt
-    wc /tmp/mimixbox/it/empty.txt
+    wc ${EMPTY_FILE}
 }
 
 TestWcReadingThreeFile() {
     export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
     export TEST_FILE_METAL=/tmp/mimixbox/it/metal.txt
     export EMPTY_FILE=/tmp/mimixbox/it/empty.txt
-    wc /tmp/mimixbox/it/empty.txt /tmp/mimixbox/it/game.txt /tmp/mimixbox/it/metal.txt
+    wc ${EMPTY_FILE} ${TEST_FILE_GAMENAME} ${TEST_FILE_METAL}
 }
--- test/spec/wc_spec.sh
+++ test/spec/wc_spec.sh
@@ -1,36 +1,5 @@
 Describe 'Word Count without options'
     Include it/textutils/wc_test.sh
-
-Setup() {
-    export TEST_DIR=/tmp/mimixbox/it
-    export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
-    export TEST_FILE_METAL=/tmp/mimixbox/it/metal.txt
-    export EMPTY_FILE=/tmp/mimixbox/it/empty.txt
-
-    mkdir -p /tmp/mimixbox/it
-
-    echo "NieR Replicant ver.1.22474487139..." > /tmp/mimixbox/it/game.txt
-    echo "NieR:Automata" >>  /tmp/mimixbox/it/game.txt
-    echo "The Legend of Zelda: Majora's Mask" >>  /tmp/mimixbox/it/game.txt
-    echo "KICHIKUOU RANCE" >>  /tmp/mimixbox/it/game.txt
-    echo "DARK SOULS" >> /tmp/mimixbox/it/game.txt
-    echo "SHADOW HEARTS" >>  /tmp/mimixbox/it/game.txt
-
-    echo "MEGADETH" > /tmp/mimixbox/it/metal.txt
-    echo "GALNERYUS" >> /tmp/mimixbox/it/metal.txt
-    echo "SYSTEM OF A DOWN" >> /tmp/mimixbox/it/metal.txt
-    
-    touch /tmp/mimixbox/it/empty.txt
-}
-
-CleanUp() {
-    export TEST_FILE_GAMENAME=/tmp/mimixbox/it/game.txt
-    export TEST_FILE_METAL=/tmp/mimixbox/it/metal.txt
-    export EMPTY_FILE=/tmp/mimixbox/it/empty.txt
-
-    rm  /tmp/mimixbox/it/empty.txt /tmp/mimixbox/it/game.txt /tmp/mimixbox/it/metal.txt
-}
-
     BeforeEach 'Setup'
     AfterEach 'CleanUp'
     It 'says "  6  16 126 /tmp/mimixbox/it/game.txt"'
