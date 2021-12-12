18d82f50a5edb49437491e75bb8141039a5a7fbe
Nao1215 <n.chika156@gmail.com>
2021-12-12 13:48:54 +0900

    Test commit for github actions

--- test/it/textutils/dos2unix_test.sh
+++ test/it/textutils/dos2unix_test.sh
@@ -20,36 +20,36 @@ Cleanup() {
 }
 
 TestDos2unixCRLF() {
-    dos2unix ${TEST_FILE1}
+    /usr/local/bin/dos2unix ${TEST_FILE1}
     file ${TEST_FILE1}
 }
 
 TestDos2unixCRLFStatus() {
-    dos2unix ${TEST_FILE1}
+    /usr/local/bin/dos2unix ${TEST_FILE1}
 }
 
 TestDos2unixThreeFileAtSameTime() {
-    dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
+    /usr/local/bin/dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
     file ${TEST_FILE1}
     file ${TEST_FILE2}
     file ${TEST_FILE3}
 }
 
 TestDos2unixThreeFileAtSameTime() {
-    dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
+    /usr/local/bin/dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
     file ${TEST_FILE1}
     file ${TEST_FILE2}
     file ${TEST_FILE3}
 }
 
 TestDos2unixThreeFileAtSameTimeStatus() {
-    dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
+    /usr/local/bin/dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
 }
 
 TestDos2unixDir() {
-    dos2unix ${TEST_DIR}
+    /usr/local/bin/dos2unix ${TEST_DIR}
 }
 
 TestDos2unixOneOfThreeFail() {
-    dos2unix ${TEST_FILE1}  ${TEST_DIR} ${TEST_FILE3} 
+    /usr/local/bin/dos2unix ${TEST_FILE1}  ${TEST_DIR} ${TEST_FILE3} 
 }
