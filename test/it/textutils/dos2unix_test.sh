dfdff8f8623452f09750dcab0d4fceb9029759dc
Nao1215 <n.chika156@gmail.com>
2021-12-15 22:50:36 +0900

    Test commit for integration tests.

--- test/it/textutils/dos2unix_test.sh
+++ test/it/textutils/dos2unix_test.sh
@@ -1,10 +1,9 @@
-export TEST_DIR=/tmp/mimixbox/it/dos2unix
-export TEST_FILE1=${TEST_DIR}/1.txt
-export TEST_FILE2=${TEST_DIR}/2.txt
-export TEST_FILE3=${TEST_DIR}/3.txt
-
-
 Setup() {
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+    export TEST_FILE2=${TEST_DIR}/2.txt
+    export TEST_FILE3=${TEST_DIR}/3.txt
+
     mkdir -p ${TEST_DIR}
     echo "abc"  > ${TEST_FILE1}
     echo "def" >> ${TEST_FILE1}
@@ -16,40 +15,68 @@ Setup() {
 }
 
 Cleanup() {
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
     rm -rf ${TEST_DIR}
 }
 
 TestDos2unixCRLF() {
-    /usr/local/bin/dos2unix ${TEST_FILE1}
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+
+    dos2unix ${TEST_FILE1}
     file ${TEST_FILE1}
 }
 
 TestDos2unixCRLFStatus() {
-    /usr/local/bin/dos2unix ${TEST_FILE1}
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+
+    dos2unix ${TEST_FILE1}
 }
 
 TestDos2unixThreeFileAtSameTime() {
-    /usr/local/bin/dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+    export TEST_FILE2=${TEST_DIR}/2.txt
+    export TEST_FILE3=${TEST_DIR}/3.txt
+
+    dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
     file ${TEST_FILE1}
     file ${TEST_FILE2}
     file ${TEST_FILE3}
 }
 
 TestDos2unixThreeFileAtSameTime() {
-    /usr/local/bin/dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+    export TEST_FILE2=${TEST_DIR}/2.txt
+    export TEST_FILE3=${TEST_DIR}/3.txt
+
+    dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
     file ${TEST_FILE1}
     file ${TEST_FILE2}
     file ${TEST_FILE3}
 }
 
 TestDos2unixThreeFileAtSameTimeStatus() {
-    /usr/local/bin/dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+    export TEST_FILE2=${TEST_DIR}/2.txt
+    export TEST_FILE3=${TEST_DIR}/3.txt
+
+    dos2unix ${TEST_FILE1} ${TEST_FILE2} ${TEST_FILE3}
 }
 
 TestDos2unixDir() {
-    /usr/local/bin/dos2unix ${TEST_DIR}
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    dos2unix ${TEST_DIR}
 }
 
 TestDos2unixOneOfThreeFail() {
-    /usr/local/bin/dos2unix ${TEST_FILE1}  ${TEST_DIR} ${TEST_FILE3} 
+    export TEST_DIR=/tmp/mimixbox/it/dos2unix
+    export TEST_FILE1=${TEST_DIR}/1.txt
+    export TEST_FILE2=${TEST_DIR}/2.txt
+    export TEST_FILE3=${TEST_DIR}/3.txt
+
+    dos2unix ${TEST_FILE1} ${TEST_DIR} ${TEST_FILE3} 
 }
