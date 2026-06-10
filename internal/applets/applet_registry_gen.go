150732ef16fdbddbf4439e3139ee4c8d94a5bd7c
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2026-06-10 18:42:52 +0900

    feat(util-linux): add eject applet to eject removable media (#383)

--- internal/applets/applet_registry_gen.go
+++ internal/applets/applet_registry_gen.go
@@ -213,6 +213,7 @@ import (
 	ap_util_linux_chattr "github.com/nao1215/mimixbox/internal/applets/util-linux/chattr"
 	ap_util_linux_chrt "github.com/nao1215/mimixbox/internal/applets/util-linux/chrt"
 	ap_util_linux_dmesg "github.com/nao1215/mimixbox/internal/applets/util-linux/dmesg"
+	ap_util_linux_eject "github.com/nao1215/mimixbox/internal/applets/util-linux/eject"
 	ap_util_linux_fallocate "github.com/nao1215/mimixbox/internal/applets/util-linux/fallocate"
 	ap_util_linux_findfs "github.com/nao1215/mimixbox/internal/applets/util-linux/findfs"
 	ap_util_linux_flock "github.com/nao1215/mimixbox/internal/applets/util-linux/flock"
@@ -251,7 +252,7 @@ import (
 // init populates the applet table. Each command is registered under its own
 // Name(), so the key can never drift from the command it dispatches to.
 func init() {
-	Applets = make(map[string]Applet, 262)
+	Applets = make(map[string]Applet, 263)
 	register(ap_archival_ar.New())
 	register(ap_archival_bunzip2.New())
 	register(ap_archival_compress.New())
@@ -477,6 +478,7 @@ func init() {
 	register(ap_util_linux_chattr.New())
 	register(ap_util_linux_chrt.New())
 	register(ap_util_linux_dmesg.New())
+	register(ap_util_linux_eject.New())
 	register(ap_util_linux_fallocate.New())
 	register(ap_util_linux_findfs.New())
 	register(ap_util_linux_flock.New())
